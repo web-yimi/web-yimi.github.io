56a6182fc3ed32b150b0b6f55419ac9ae2ee4b94
web-yimi <1370796703@qq.com>
2016-12-08 23:54:06 +0800

    zhuye

--- js/myblog.js
+++ js/myblog.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	//轮播
 	var n=0;
 	var next=0;
 	var t=setInterval(move,2000);
@@ -6,7 +7,7 @@ $(document).ready(function(){
 	var flag=true;
 	function move(type){
 		var type=type||'right';
-		if(type='right'){
+		if(type=='right'){
 			if(!flag){
 				return;
 			}
@@ -21,7 +22,7 @@ $(document).ready(function(){
 				flag=true;
 			});
 			n=next;
-		}else if(type='left'){
+		}else if(type=='left'){
 			if(!flag){
 				return;
 			}
@@ -49,16 +50,53 @@ $(document).ready(function(){
 	$('.toright').click(function(){
 		move('right')
 	})
+	//楼层跳转
 	$('.nav-home').click(function(){
 		$('body,html').animate({scrollTop:0},1000);
 	})
-	$('.nav-about').click(function(){
-		document.body.animate({scrollTop:$('.about').position().top})
-		document.documentElement.animate({scrollTop:$('.about').position().top})
+	for(var i=0;i<jQuery('.section').length;i++){
+		var floorTop=jQuery('.section').eq(i).position();
+		jQuery('.section').eq(i).prop('h',floorTop.top);
+	}
+	$('.nav-list').click(function(){
+		var index=$(this).index();
+		var hh=$(".section").eq(index).prop("h");
+		var top=$(window).scrollTop();
+		var src={aa:top};
+		$(src).animate({aa:hh},{
+			duration:300,
+			step:function(){
+				$(window).scrollTop(src.aa)
+			}
+		})
 	})
-	$('.nav-work').click(function(){
-		
+	$(window).scroll(function(){
+		var CHeight=$(".section").eq(3).position().top;
+		var top=$(document).scrollTop();
+		if(top>CHeight-400){
+			$(".contact .contact-con .left").addClass("active")
+			$(".contact .contact-con .right").addClass("active")
+		}
 	})
+	
+	//work选项卡
+	$(".work-nav li").each(function(index){
+		$(this).on("click",function(){
+			$(".work-lists").each(function(index){
+				$(this).hide();
+			})
+			$(".work-lists").eq(index).show()
+		})
+	})
+	
+	//开始页面
+	var cW=$(document).width();
+	var height=$(window).height();
+	$(".start").css('height',height);
+	var li_width=cW/6;
+	var length=$(".start ul li").length;
+	$(".start ul").css("width",li_width*length);
+	
 })
 
 
